0e7a0eed6513af422d3e49d4fa7231e1bad2dc0b
William Ray Noble Jr <wrnoblejrsbts@gmail.com>
2020-06-05 17:47:28 -0400

    try catch included for mistakes

--- app.js
+++ app.js
@@ -6,21 +6,26 @@ function printMessage(username, badgeCount, points) {
 }
 
 function getProfile(username) {
-    //Connect to the API URL (https://teamtreehouse.com/williamnoble.json)
-    const request = https.get(`https://teamtreehouse.com/${username}.json`, response => {
-        let body = "";
-        //Read the data
-        response.on('data', data => {
-            body += data.toString();
-        });
+    try{
+        //Connect to the API URL (https://teamtreehouse.com/williamnoble.json)
+        const request = https.get(`https://teamtreehouse.com/${username}.json`, response => {
+            let body = "";
+            //Read the data
+            response.on('data', data => {
+                body += data.toString();
+            });
 
-        response.on('end', () => {
-            const profile =JSON.parse(body);
-            printMessage(username, profile.badges.length, profile.points.JavaScript);
+            response.on('end', () => {
+                const profile =JSON.parse(body);
+                printMessage(username, profile.badges.length, profile.points.JavaScript);
+            });
+            //Parse the data
+            //Print the data
         });
-        //Parse the data
-        //Print the data
-    });
+        request.on('error', error => console.error(`Problem(s) with request: ${error.message}`));
+    }catch(error) {
+        console.error(error.message);
+    }
 };
 
 const users = ["williamnoble", "chalkers", "alenaholligan"];
