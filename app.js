47ec9083c42c212740d882539bbb61a96a2fbff1
William Ray Noble Jr <wrnoblejrsbts@gmail.com>
2020-06-05 17:39:36 -0400

    it worked connected to treehouse API

---
+++ app.js
@@ -0,0 +1,28 @@
+const https = require('https');
+
+function printMessage(username, badgeCount, points) {
+    const message = `${username} has ${badgeCount} total badge(s) and ${points} in JavaScript.`;
+    console.log(message);
+}
+
+function getProfile(username) {
+    //Connect to the API URL (https://teamtreehouse.com/williamnoble.json)
+    const request = https.get(`https://teamtreehouse.com/${username}.json`, response => {
+        let body = "";
+        //Read the data
+        response.on('data', data => {
+            body += data.toString();
+        });
+
+        response.on('end', () => {
+            const profile =JSON.parse(body);
+            printMessage(username, profile.badges.length, profile.points.JavaScript);
+        });
+        //Parse the data
+        //Print the data
+    });
+};
+
+const users = ["williamnoble", "chalkers", "alenaholligan"];
+
+users.forEach(getProfile);
